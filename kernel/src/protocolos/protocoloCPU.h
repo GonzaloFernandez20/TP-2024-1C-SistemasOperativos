2be4f1b5402addc33359fbb9e3b2e6be6e8cb589
GonzaloFernandez20 <lfernandezbogarin@frba.utn.edu.ar>
2024-05-24 04:17:04 +0000

    Protocolo CPU

--- kernel/src/protocolos/protocoloCPU.h
+++ kernel/src/protocolos/protocoloCPU.h
@@ -2,8 +2,9 @@
 #define PROT_CPU_H_INCLUDED
 
 // ---------------- LIBRERIAS 
+#include <utils/serializacion/serializacion.h>
+#include <include/estructuras.h>
 #include <planificacion/pcb.h>
-
 
 // ---------- DECLARACIONES DE FUNCIONES
 void enviar_pcb(t_pcb *pcb_envio);
