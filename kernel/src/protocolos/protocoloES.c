2d7f9ab0d3a1d4fd24c303c29ea54e94ae53fb9c
MeliBugallo <melinabugallo@gmail.com>
2024-05-25 15:33:41 +0000

    kernel servidor

--- kernel/src/protocolos/protocoloES.c
+++ kernel/src/protocolos/protocoloES.c
@@ -19,7 +19,7 @@ void *procesar_conexion_es(void *nombre_interfaz){
 		case -1:
 
 			pthread_mutex_lock(&mutex_log_debug);
-			log_error(kernel_log_debugg, "ENTRADA/SALIDA se desconecto\n");
+			log_error(kernel_log_debugg, "%s se desconecto\n", nombre);
 			pthread_mutex_unlock(&mutex_log_debug);
 			sacar_interfaz_de_diccionario(nombre);
             cliente_conectado = 0;
@@ -39,13 +39,22 @@ void sacar_interfaz_de_diccionario(char* nombre_interfaz){
 
 	t_interfaz* interfaz = dictionary_remove(interfaces_conectadas, nombre_interfaz);
 
+	mandar_procesos_a_exit(interaz->bloqueados)
 	free(interfaz->bloqueados->nombre);
-	//pasa los procesos bloqueados en la cola a exit
 	list_destroy(interfaz->bloqueados->cola);
 	pthread_mutex_destroy(&interfaz->bloqueados->mutex_cola);
 	free(interfaz->tipo);
 	free(interfaz->bloqueados);
 	free(interfaz);
+}
+
+void mandar_procesos_a_exit(t_estado* bloqueados){
+	int cantidad_procesos_bloqueados = list_size(bloqueados->cola);
+	
+	for(int i=0; i<cantidad_procesos_bloqueados; i++){
+		t_pcb* PCB = list_get(bloqueados->cola, 0);
+		trasladar(PCB->pid, bloqueados, estado_exit);
+	}
 }
 
 void solicitar_operacion_IO_GEN_SLEEP(int PID, int fd, int parametro){
@@ -73,7 +82,7 @@ void recibir_aviso(char* nombre, int fd){
 	log_info(kernel_log_debugg, "Operacion %s completada", nombre);
 	pthread_mutex_unlock(&mutex_log_debug);
 	
-		//sabiendo el pid buscas en el dicc con la nombre y accedes a su t_estado 
-		// buscar en su cola el pid y pasarlo de blocked a ready
+	t_interfaz* interfaz = dictionary_get(interfaces_conectadas, nombre);
+	trasladar(PID, interfaz->bloqueados, ready);
 }
 
--- kernel/src/protocolos/protocoloES.h
+++ kernel/src/protocolos/protocoloES.h
@@ -2,6 +2,8 @@
 #define KERNEL_PROTOCOLO_H_
 
 #include <include/estructuras.h>
+#include <planificacion/pcb.h>
+#include <planificacion/colasEstados.h>
 
 #include <utils/hilos/hilos.h>
 #include <utils/serializacion/serializacion.h>
@@ -9,6 +11,8 @@
 void *procesar_conexion_es(void *nombre_interfaz);
 
 void sacar_interfaz_de_diccionario(char* nombre_interfaz);
+
+void mandar_procesos_a_exit(t_estado* bloqueados);
 
 void solicitar_operacion_IO_GEN_SLEEP(int PID, int fd, int parametro);
 
